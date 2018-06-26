66aa881baf9ef939ab3f37a258f97254b03a9fd7
Unknown <rispir45@gmail.com>
2018-06-26 11:03:41 -0400

    Created html and js code for form
    Added all the html and js code for creating the form and changing the header based on the input.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const button = document.querySelector('button')
-
 const updateText = function() {
     const heading = document.querySelector('#pratt')
     heading.textContent = "Chris Pratt is #1 Chris"
 }
-
 button.onclick = updateText
+
+const frm1 = document.querySelector('#frm1')
+function formFunction() {
+    const heading = document.querySelector('#pageTitle')
+    heading.textContent = document.querySelector('.textV').value
+    event.preventDefault()
+}
+frm1.addEventListener('submit', formFunction)
+
