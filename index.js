56c21cb9461bcf07ca464a08cf0864ed2d72ad3a
Unknown <rispir45@gmail.com>
2018-06-26 09:33:48 -0400

    Added code for first button functionality
    Added onClick code for first button so that it changes our h1.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-console.log('It works!')
-console.warn('Omar coming!')
-console.error('Aw, snap!')
+const button = document.querySelector('button')
+
+const updateText = function() {
+    const heading = document.querySelector('#pageTitle')
+    heading.textContent = "Chris-a-palooza"
+}
+
+button.onclick = updateText
