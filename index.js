494ce524dc23877cdaf1124c173af8721afcfff5
Unknown <rispir45@gmail.com>
2018-06-26 15:52:24 -0400

    Reformatting index.js and reset text box
    Added functionality to empty the text input box after you submit.

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const button = document.querySelector('button') //create variable that calls our button
-const updateText = function() {
-    const heading = document.querySelector('#pratt') //create variable that calls the Chris Pratt header
-    heading.textContent = "Chris Pratt is #1 Chris" //changes the text of Chris Pratt header
+const updateText = function () {
+	const heading = document.querySelector('#pratt') //create variable that calls the Chris Pratt header
+	heading.textContent = "Chris Pratt is #1 Chris" //changes the text of Chris Pratt header
 }
 button.onclick = updateText //when button is clicked call updateText
 
 
 const frm1 = document.querySelector('#frm1') //create variable that calls our form
 function formFunction() {
-    const heading = document.querySelector('#pageTitle') //create value that calls the page header
-    heading.textContent = document.querySelector('.textV').value //change text of header with forms text line
-    event.preventDefault() //stops page from refreshing after submit
+	const heading = document.querySelector('#pageTitle') //create value that calls the page header
+	heading.textContent += document.querySelector('.textV').value //change text of header with forms text line
+	event.preventDefault() //stops page from refreshing after submit
+	document.querySelector('.textV').value = ''
 }
 frm1.addEventListener('submit', formFunction) //event listener to detect submit and call formFunction
 
