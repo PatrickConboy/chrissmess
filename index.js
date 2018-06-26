a9181b941e6cf08b3feae4d5b63103c4dbc590df
Unknown <rispir45@gmail.com>
2018-06-26 09:44:48 -0400

    Update button code to change 2nd header
    Update the button code in our index.js file to change the text of the 2nd header.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const button = document.querySelector('button')
 
 const updateText = function() {
-    const heading = document.querySelector('#pageTitle')
-    heading.textContent = "Chris-a-palooza"
+    const heading = document.querySelector('#pratt')
+    heading.textContent = "Chris Pratt is #1 Chris"
 }
 
 button.onclick = updateText
