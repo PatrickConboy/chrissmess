6b5dfb216722c4a9600528b4a79862dab3ce787f
Unknown <rispir45@gmail.com>
2018-06-27 19:01:03 -0400

    Formatting changes
    Last stable commit before I make some major changes. Might break my javascript.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// class App {
+// 	constructor() {
+// 		const form = document.querySelector('form#flickForm')
+// 		form.addEventListener('submit', handleSubmit)
+// 	}
+// }
+
+// const app = new App()
+
 const form = document.querySelector('form#flickForm')  // select my form element and assign a variable
 
 // This function takes the strings entered in my text fields, 
@@ -7,21 +16,14 @@ const createSpanLi = function (flickItem, heroItem)  {
 	const list = document.querySelector('#flicks')
 	const item = document.createElement('li')
 
-	// This block fo code creates a span, puts our flick text into it, and styles it.
 	const flickSpan = document.createElement('SPAN')
 	flickSpan.append(flickItem)
 	changeSpanColor(flickSpan, 'green')
-	flickSpan.style.fontFamily = 'Parisienne, cursive'
-	flickSpan.style.fontSize = '50px'
 
-	// This block of code creates a span, puts our hero text into it, and styles it.
 	const heroSpan = document.createElement('SPAN')
 	heroSpan.append(heroItem)
 	changeSpanColor(heroSpan, 'black')
-	heroSpan.style.fontFamily = 'Parisienne, cursive'
-	heroSpan.style.fontSize = '50px'
 
-	// This block of code simply takes our created spans and puts them in our list, with a space between.
 	item.append(flickSpan)
 	item.append('   ')
 	item.append(heroSpan)
@@ -36,20 +38,29 @@ const changeSpanColor = function (span, color) {
 // This function takes our form and displays the text entered in our
 // text fields in a list format, with the help of a few other functions.
 // Namely: changeSpanColor() and createSpanLi()
-const updateFlickList = function (ev) {
+const handleSubmit = function (ev) {
 	
-	ev.preventDefault() // Stops page from refreshing when form is submitted. Form submit refreshes page by default.
-	const f = ev.target // This line targets our event (form) and gives it a variable that we can call on.
+	ev.preventDefault() 
+	const f = ev.target 
 
-	// This block of code obtains the text entered into our flick and hero fields.
-	// It then calls createSpanLi() to put our text into spans and append them to a list to be displayed.
+	// const flick = {
+	// 	name: f.flickName.value,
+	// 	hero: f.chrisName.value
+	// }
+
+	// const item = renderItem(flick)
+
+	// const list = document.querySelector('#flicks')
+	// list.appendChild(item)
+
 	const flickItem = f.flickName.value
 	const heroItem = f.favoriteSuperHero.value
 	createSpanLi(flickItem, heroItem)
 
 	f.reset()
+	f.flickName.focus()
 }
 
 // This event listener calls our updateFlickList function when our form's submit
 // button is clicked.
-form.addEventListener('submit', updateFlickList)
+form.addEventListener('submit', handleSubmit)
