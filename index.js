dd403d6d537e3ec9d9222a19cd1142e942f3b3ef
Unknown <rispir45@gmail.com>
2018-06-27 10:03:20 -0400

    Added new form element and coded createLi function
    Added my new form element and created a new function in index.js that breaks up our original function into 2 parts.

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const form = document.querySelector('form#flickForm')
 
+const createLi = function(listItem) {
+	const list = document.querySelector('#flicks')
+	const item = document.createElement('li')
+	item.textContent = listItem
+	list.appendChild(item)
+}
+
 const updateFlickList = function(ev) {
   ev.preventDefault()
   const f = ev.target
 
-  const flickName = f.flickName.value
+  const listItem = f.flickName.value
 
-  const list = document.querySelector('#flicks')
-  const item = document.createElement('li')
-  item.textContent = flickName
-  list.appendChild(item)
+  createLi(listItem)
 
   f.reset()
 }
