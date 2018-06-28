d3e5c39579c81a37f60e9f660ed1df327cd3380e
Unknown <rispir45@gmail.com>
2018-06-28 00:31:43 -0400

    Per recommendation, copied current code from github
    I was working on updating my code to put everything into a class and have it setup like we did in class, but was told the homework was very hard and I should just skip that and get started on the homework.

--- index.js
+++ index.js
@@ -1,66 +1,52 @@
-// class App {
-// 	constructor() {
-// 		const form = document.querySelector('form#flickForm')
-// 		form.addEventListener('submit', handleSubmit)
-// 	}
-// }
-
-// const app = new App()
-
-const form = document.querySelector('form#flickForm')  // select my form element and assign a variable
-
-// This function takes the strings entered in my text fields, 
-// puts them into spans, changes their color/font, and then
-// appends them to the list.
-const createSpanLi = function (flickItem, heroItem)  {
-	const list = document.querySelector('#flicks')
-	const item = document.createElement('li')
-
-	const flickSpan = document.createElement('SPAN')
-	flickSpan.append(flickItem)
-	changeSpanColor(flickSpan, 'green')
-
-	const heroSpan = document.createElement('SPAN')
-	heroSpan.append(heroItem)
-	changeSpanColor(heroSpan, 'black')
-
-	item.append(flickSpan)
-	item.append('   ')
-	item.append(heroSpan)
-	list.appendChild(item)
-}
-
-// This function takes a span and color and changes the span to the given color.
-const changeSpanColor = function (span, color) {
-	span.style.color = color
-}
-
-// This function takes our form and displays the text entered in our
-// text fields in a list format, with the help of a few other functions.
-// Namely: changeSpanColor() and createSpanLi()
-const handleSubmit = function (ev) {
-	
-	ev.preventDefault() 
-	const f = ev.target 
-
-	// const flick = {
-	// 	name: f.flickName.value,
-	// 	hero: f.chrisName.value
-	// }
-
-	// const item = renderItem(flick)
-
-	// const list = document.querySelector('#flicks')
-	// list.appendChild(item)
-
-	const flickItem = f.flickName.value
-	const heroItem = f.favoriteSuperHero.value
-	createSpanLi(flickItem, heroItem)
-
-	f.reset()
-	f.flickName.focus()
-}
-
-// This event listener calls our updateFlickList function when our form's submit
-// button is clicked.
-form.addEventListener('submit', handleSubmit)
+class App {
+	constructor() {
+	  const form = document.querySelector('form#flickForm')
+	  form.addEventListener('submit', (ev) => {
+		ev.preventDefault()
+		this.handleSubmit(ev)
+	  })
+	}
+  
+	renderProperty(name, value) {
+	  const span = document.createElement('span')
+	  span.classList.add(name)
+	  span.textContent = value
+	  return span
+	}
+  
+	renderItem(flick) {
+	  const item = document.createElement('li')
+	  item.classList.add('flick')
+  
+	  // get the list of properties
+	  const properties = Object.keys(flick)
+  
+	  // loop over each property
+	  properties.forEach((propertyName) => {
+		// build a span
+		const span = this.renderProperty(propertyName, flick[propertyName])
+		item.appendChild(span)
+	  })
+  
+	  return item
+	}
+  
+	handleSubmit(ev) {
+	  const f = ev.target
+  
+	  const flick = {
+		name: f.flickName.value,
+		chris: f.chrisName.value,
+	  }
+  
+	  const item = this.renderItem(flick)
+  
+	  const list = document.querySelector('#flicks')
+	  list.appendChild(item)
+  
+	  f.reset()
+	  f.flickName.focus()
+	}
+  }
+  
+  const app = new App()
